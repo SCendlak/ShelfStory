8e8e6eeeed0440f214f07c3c777a83dcdb87b83e
Stan Cendlak <stancendlak@gmail.com>
2023-11-27 19:18:04 +0100

    Shitty redirect when 502 and mocked postman server

---
+++ shelf-story-front/src/app/badGateway/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function badGateway(){
+    return(
+        <main>
+            <h1>BADE GATEWAY</h1>
+            <Link href="/">GO HOME BITCH</Link>
+        </main>
+    )
+}
--- shelf-story-front/src/app/page.tsx
+++ shelf-story-front/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Link from "next/link";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
         <Image src={'/favicon.ico'} alt={'LOGO'} width={50} height={50} />
+
+        <Link href="/test">TEST LINK</Link>
     </main>
   )
 }
